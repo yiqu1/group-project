555e249473c126ee35c4accb943d26e5859aed37
Yi Qu <yqu82888@gmail.com>
2023-04-28 09:20:07 -0400

    Create a Virtual Network in main.tf & added providers.tf

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # group-project
+*1. Create a Resource Group*
+*2. Create a Virtual Network*
--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
